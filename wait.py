d757e17b470905f124b854abcfd7f9acbe4cc267
Matthew Herber <matthewherber@sbcglobal.net>
2025-09-01 18:44:03 -0400

    test

--- wait.py
+++ wait.py
@@ -15,13 +15,13 @@ print(f"Authenticated to repo {repo.full_name}", flush=True)
 
 def checkIfSafeToProceed(status):
     #check if there are any other workflow runs for this workflow that are queued or in progress with an ID less than us
-    workflow_runs = repo.get_workflow_runs(status=status)
+    workflow_runs = repo.get_workflow_runs(status=status, exclude_pull_requests=False)
+    #print all runs
+    for wr in workflow_runs:
+        print(f"Found workflow run {wr.name} with ID {wr.id} and status {wr.status}", flush=True)
     que_workflow_runs = [wr for wr in workflow_runs if wr.name == WORKFLOW_NAME]
     any_before_us = any(wr.id < RUN_ID for wr in que_workflow_runs)
     return not any_before_us
-
-#now find the workflow run list for the que workflow, and see if there is any before us
-workflow_runs = repo.get_workflow_runs()
 
 #loop and get all their IDs, and check if ANY are less than us
 while True:
