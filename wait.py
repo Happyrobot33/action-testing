3d1601d86a2328bfc94c99bc57067d3caf535f81
Matthew Herber <matthewherber@sbcglobal.net>
2025-09-01 17:37:44 -0400

    iteration

--- wait.py
+++ wait.py
@@ -9,6 +9,7 @@ import os
 github_token = os.getenv("GITHUB_TOKEN")
 repo_name = os.getenv("GITHUB_REPOSITORY")
 workflow_run_id = int(os.getenv("WORKFLOW_RUN_ID"))
+workflow_name = os.getenv("WORKFLOW_NAME")
 
 auth = Auth.Token(github_token)
 g = Github(auth=auth)
@@ -16,19 +17,18 @@ repo = g.get_repo(repo_name)
 print(f"Authenticated to repo {repo.full_name}")
 
 #now find the workflow run list for the que workflow, and see if there is any before us
-workflows_running = repo.get_workflow_runs(status="in_progress")
-#also get any qued
-workflows_queued = repo.get_workflow_runs(status="queued")
-all_workflows = workflows_running + workflows_queued
+workflow_runs = repo.get_workflow_runs()
 
 #loop and get all their IDs, and check if ANY are less than us
-ids = [w.id for w in all_workflows]
-print(f"Found {len(ids)} queued or running workflow runs: {ids}")
-while any(wid < workflow_run_id for wid in ids):
+while True:
+    workflow_runs = repo.get_workflow_runs()
+    #get all the ones running the specific workflow
+    que_workflow_runs = [wr for wr in workflow_runs if wr.name == workflow_name and wr.status in ["queued", "in_progress"]]
+    #check them all for if any of them have an ID less than us
+    any_before_us = any(wr.id < workflow_run_id for wr in que_workflow_runs)
+    if not any_before_us:
+        print("No workflow runs queued or running before us, proceeding...")
+        break
+
     print("There are still workflow runs queued or running before us, waiting 10 seconds...")
     time.sleep(10)
-    workflows_running = repo.get_workflow_runs(status="in_progress")
-    workflows_queued = repo.get_workflow_runs(status="queued")
-    all_workflows = workflows_running + workflows_queued
-    ids = [w.id for w in all_workflows]
-    print(f"Found {len(ids)} queued or running workflow runs: {ids}")
